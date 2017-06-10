44e48e44d57941a9dfc07ebde2431c80143c21df
fhightower <floyd.hightower27@gmail.com>
2017-06-10 13:43:55 -0300

    Making make_get_request function private

--- xn_twist_sdk/xnTwist.py
+++ xn_twist_sdk/xnTwist.py
@@ -13,7 +13,7 @@ class xnTwist(object):
     def __init__(self):
         self.api_path = "http://miceandmen.tk:5000/"
 
-    def make_get_request(self, url):
+    def _make_get_request(self, url):
         """Make and handle a ``GET`` request to the given url."""
         response = requests.get(url)
 
@@ -45,33 +45,33 @@ class xnTwist(object):
 
     def get_base(self):
         """Make ``GET`` request to the base api branch (``/``)."""
-        return self.make_get_request(self.api_path)
+        return self._make_get_request(self.api_path)
 
     def get_administrators(self):
         """
         Make ``GET`` request to branch with admin users (``/administrators``).
         """
-        return self.make_get_request(self.api_path + "administrators")
+        return self._make_get_request(self.api_path + "administrators")
 
     def get_feed(self):
         """
         Make ``GET`` request to branch with recently classified characters
         (``/feed``).
         """
-        return self.make_get_request(self.api_path + "feed")
+        return self._make_get_request(self.api_path + "feed")
 
     def get_mappings(self):
         """
         Make ``GET`` request to branch with character mappings (``/mappings``).
         """
-        return self.make_get_request(self.api_path + "mappings")
+        return self._make_get_request(self.api_path + "mappings")
 
     def get_non_basic_chars(self):
         """
         Make ``GET`` request to branch with the list of non-basic characters
         that may be used to spoof latin characters (``/non_basic_characters``).
         """
-        return self.make_get_request(self.api_path + "non_basic_characters")
+        return self._make_get_request(self.api_path + "non_basic_characters")
 
     def get_unmapped_chars(self):
         """
@@ -79,25 +79,25 @@ class xnTwist(object):
         have not been mapped to any basic characters
         (``/unmapped_characters``).
         """
-        return self.make_get_request(self.api_path + "unmapped_characters")
+        return self._make_get_request(self.api_path + "unmapped_characters")
 
     def get_basic_chars(self):
         """
         Make ``GET`` request to branch with basic characters
         (``/basic_characters``).
         """
-        return self.make_get_request(self.api_path + "basic_characters")
+        return self._make_get_request(self.api_path + "basic_characters")
 
     def get_suggested_deprecations(self):
         """
         Make ``GET`` request to branch with non-basic characters that have
         been suggested for deprecation (``/suggested_deprecations``).
         """
-        return self.make_get_request(self.api_path + "suggested_deprecations")
+        return self._make_get_request(self.api_path + "suggested_deprecations")
 
     def get_depricated_chars(self):
         """
         Make ``GET`` request to branch with the non-basic characters that have
         been deprecated (``/depricated_characters``).
         """
-        return self.make_get_request(self.api_path + "depricated_characters")
+        return self._make_get_request(self.api_path + "depricated_characters")
