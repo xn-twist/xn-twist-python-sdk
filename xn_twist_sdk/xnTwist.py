c7c620d5f91aab5b78c2121351e7d67e652e80f7
fhightower <floyd.hightower27@gmail.com>
2017-06-10 12:37:44 -0300

    Making updates for documentation

--- xn_twist_sdk/xnTwist.py
+++ xn_twist_sdk/xnTwist.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Python SDK for xn-twist's API."""
+"""Python SDK for XN-Twist's API."""
 
 import json
 
@@ -8,7 +8,7 @@ import requests
 
 
 class xnTwist(object):
-    """Class for communicating with the xn-twist API."""
+    """Class for communicating with the XN-Twist API."""
 
     def __init__(self):
         self.api_path = "http://miceandmen.tk:5000/"
@@ -23,28 +23,60 @@ class xnTwist(object):
             raise RuntimeWarning("Error response from {}".format(url))
 
     def get_base(self):
+        """Make ``GET`` request to the base api branch (``/``)."""
         return self.make_request(self.api_path)
 
     def get_administrators(self):
+        """
+        Make ``GET`` request to branch with admin users (``/administrators``).
+        """
         return self.make_request(self.api_path + "administrators")
 
     def get_feed(self):
+        """
+        Make ``GET`` request to branch with recently classified characters
+        (``/feed``).
+        """
         return self.make_request(self.api_path + "feed")
 
     def get_mappings(self):
+        """
+        Make ``GET`` request to branch with character mappings (``/mappings``).
+        """
         return self.make_request(self.api_path + "mappings")
 
     def get_non_basic_chars(self):
+        """
+        Make ``GET`` request to branch with the list of non-basic characters
+        that may be used to spoof latin characters (``/non_basic_characters``).
+        """
         return self.make_request(self.api_path + "non_basic_characters")
 
     def get_unmapped_chars(self):
+        """
+        Make ``GET`` request to branch with list of non-basic characters that
+        have not been mapped to any basic characters
+        (``/unmapped_characters``).
+        """
         return self.make_request(self.api_path + "unmapped_characters")
 
     def get_basic_chars(self):
+        """
+        Make ``GET`` request to branch with basic characters
+        (``/basic_characters``).
+        """
         return self.make_request(self.api_path + "basic_characters")
 
     def get_suggested_deprecations(self):
+        """
+        Make ``GET`` request to branch with non-basic characters that have
+        been suggested for deprecation (``/suggested_deprecations``).
+        """
         return self.make_request(self.api_path + "suggested_deprecations")
 
     def get_depricated_chars(self):
+        """
+        Make ``GET`` request to branch with the non-basic characters that have
+        been deprecated (``/depricated_characters``).
+        """
         return self.make_request(self.api_path + "depricated_characters")
