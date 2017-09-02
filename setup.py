3d301f8f39854543520f7e34035a233df0e024e3
Floyd Hightower <floyd.hightower27@gmail.com>
2017-09-02 16:49:35 -0300

    Bump version: 1.2.4 → 2.0.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='xntwist_python_sdk',
-    version='1.2.4',
+    version='2.0.0',
     description="Python SDK for XN-Twist's API",
     long_description=readme + '\n\n' + history,
     author="Floyd Hightower",
